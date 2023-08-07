57ed86dfec3c61e10a858d32c70caba796df6d9c
Lshruthi <shruthil754@gmail.com>
2023-08-07 22:36:07 +0530

    first commit

---
+++ Script.sql
@@ -0,0 +1,15 @@
+ -- select * from aug_b3.employee;
+--  select * from aug_b3.customer;
+
+select * from product;
+
+-- +------------+--------------+-------+-------+
+-- | product_id | product_name | price | stock |
+-- +------------+--------------+-------+-------+
+-- |          1 | Smartphone   | 25000 |   100 |
+-- |          2 | Laptop       | 60000 |    50 |
+-- |          3 | Tablet       | 15000 |    75 |
+-- |          4 | Headphones   |  2000 |   200 |
+-- |          5 | Smartwatch   | 12000 |    40 |
+-- +------------+--------------+-------+-------+
+
